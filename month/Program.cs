4d4d4729bc861ad944d358dcc16b1ccb845c127a
aslancoder <6lzkmnp@code.edu.az>
2022-03-07 23:50:54 +0400

    Proje dosyası ekle.

---
+++ month/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace month
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ay = "aslan";
+            if (ay == "Yanvar" || ay == "Fevral" || ay == "Deakabr")
+            {
+                Console.WriteLine("winter");
+            }
+            else if (ay == "Iyun" || ay == "Iyul" || ay == "Avqust")
+            {
+                Console.WriteLine("summer");
+            }
+            else if (ay == "Mart" || ay == "aprel" || ay == "May")
+            {
+                Console.WriteLine("spring");
+            }
+            else if (ay == "sentyabr" || ay == "oktyabr" || ay == "Noyabr")
+            {
+                Console.WriteLine("autumn");
+            }
+            else
+                Console.WriteLine("XAIS EDIREM AYLARIN ADINI OYREN");
+
+        }
+        
+
+
+    }
+}
